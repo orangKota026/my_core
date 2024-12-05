d42fca3a6b2a2d11c9f75fe35c8488f76a24530a
orangKota026 <projectbasuni@gmail.com>
2024-12-06 01:46:24 +0700

    Updates

---
+++ assets/axios.8c2c6a12.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.a45e5f44.js";import{a}from"./axios.f7d2ff76.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
